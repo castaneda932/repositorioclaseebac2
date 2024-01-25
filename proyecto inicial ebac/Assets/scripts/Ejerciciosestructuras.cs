df4749d789cfb9368f3acbd5391eede130faa8b6
castaneda932 <castachatz90@outlook.com>
2024-01-25 02:43:15 -0600

    Terminamos las 2 actividades restantes
    -corregí  la 2da actividad.
    -realice la actividad 3 y 4

--- proyecto inicial ebac/Assets/scripts/Ejerciciosestructuras.cs
+++ proyecto inicial ebac/Assets/scripts/Ejerciciosestructuras.cs
@@ -1,32 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Ejerciciosestructuras : MonoBehaviour
 {
 
     List<int> miLista= new List<int>();
-    int[] enteros = { 1, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30 };
-    List<int> ordenadosDescendente = new List<int>();
+    int[] enteros = { 14, 32, 56, 19, 12, 85, 8, 21, 52, 27, 31 };
+    int[] EnterosOrdenados;
+    List<int> listaNumeros = new List<int> { 1, 2, 3, 4, 5, 2, 3, 6, 7, 8, 1 };
+    HashSet<int> listaSinRepetidos;
+    Stack<string> pila = new Stack<string>();
+    Queue<string> cola = new Queue<string>();
+
+
 
 
     void Start()
     {
+        //esto es para llamar a consola el arreglo de enteros de la funcion arreglos
+        EnterosOrdenados = Arreglos(enteros);
+        foreach (int entero in EnterosOrdenados) 
+        {
+         Debug.Log(entero);
+        }
+        //esto es para llamar a consola el hashet sin duplicados
+        listaSinRepetidos = CreaHashSetSinDuplicados(listaNumeros);
+        foreach (int entero in listaSinRepetidos)
+        {
+            Debug.Log(entero);
+        }
 
+        ListaDeStrings();
         
-        Arreglos();
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     void listaDeNumeros(int tamaño = 50, int rangoInf = 0, int rangoSup = 50)
     {
-
         for (int i = 0; i < tamaño; i++)
         {
             miLista.Add(Random.Range(rangoInf, rangoSup));
@@ -34,35 +46,66 @@ public class Ejerciciosestructuras : MonoBehaviour
         foreach (var numero in miLista)
         {
             Debug.Log(numero);
-        }
-
-        
+        }      
     }
-    void Arreglos()
+    int[] Arreglos(int[] arregloDeEnteros)
     {
-       for (int i = 0; i < enteros.Length; i++)
-        {
-            ordenadosDescendente.Add(i);
-        }
-       foreach(var numero in ordenadosDescendente)
-        {
-            Debug.Log(numero);
-        }
-       //reverse se usa para ordenar la lista previa de manera descendente
-       ordenadosDescendente.Reverse();
-       foreach (var numero in ordenadosDescendente)
-        {
-            Debug.Log(numero);
-        }
+        //esta es la manera de ordenar una lista array de manera descendente
+        int[] arregloDeEnterosOrdenados = arregloDeEnteros.OrderByDescending(enteros => enteros).ToArray();
 
-       
-
-        
+        return arregloDeEnterosOrdenados;    
     }
+    //funcion para crear un hashet
+    HashSet<T> CreaHashSetSinDuplicados<T>(List<T> inputList)
+    {
+        // Utilizando LINQ para eliminar duplicados y crear un HashSet
+        return new HashSet<T>(inputList.Distinct());
 
-   
+    }
+    void ListaDeStrings() 
+    {
+        pila.Push("Uno");
+        pila.Push("Dos");
+        pila.Push("Tres");
+        Debug.Log(pila.Peek());
+        pila.Pop();
+        Debug.Log(pila.Peek());
+        pila.Pop();
+        Debug.Log(pila.Peek());
+        pila.Pop();
+
+        cola.Enqueue("Uno");
+        cola.Enqueue("Dos");
+        cola.Enqueue("Tres");
+        Debug.Log(cola.Peek());
+        cola.Dequeue();
+        Debug.Log(cola.Peek());
+        cola.Dequeue();
+        Debug.Log(cola.Peek());
+        cola.Dequeue();
+
+
+
+
+    }
     
-    
+
+
+
+
+
+
+
 
 
 }
+
+
+
+
+
+
+
+
+
+ 
